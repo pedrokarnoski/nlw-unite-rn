826527de489612495eda9a37b78564d1c1963a6e
Pedro Henrique Karnoski <pedrokarnoski@gmail.com>
2024-04-01 22:22:11 -0300

    feat: Register screen

---
+++ src/app/register.tsx
@@ -0,0 +1,50 @@
+import { View, Image, StatusBar } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { Link } from "expo-router";
+
+import { Input } from "@/components/input";
+import { Button } from "@/components/button";
+
+import { colors } from "@/styles/colors";
+
+export default function Register() {
+  return (
+    <View className="flex-1 bg-green-500 items-center justify-center p-8">
+      <StatusBar barStyle="light-content" />
+
+      <Image
+        source={require("@/assets/logo.png")}
+        className="h-16"
+        resizeMode="contain"
+      />
+      <View className="w-full mt-12 gap-3">
+        <Input>
+          <MaterialCommunityIcons
+            name="account-circle-outline"
+            size={20}
+            color={colors.green[200]}
+          />
+          <Input.Field placeholder="Nome completo" />
+        </Input>
+
+        <Input>
+          <MaterialCommunityIcons
+            name="email-outline"
+            size={20}
+            color={colors.green[200]}
+          />
+          <Input.Field placeholder="E-mail" keyboardType="email-address" />
+        </Input>
+
+        <Button title="Realizar inscrição" />
+
+        <Link
+          href="/"
+          className="text-gray-100 text-base font-bold text-center mt-2"
+        >
+          Já possui ingresso?
+        </Link>
+      </View>
+    </View>
+  );
+}
