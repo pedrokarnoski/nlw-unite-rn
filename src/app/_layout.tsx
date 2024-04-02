68773ea5e42fbc60e761cc3fe271b8568964f392
Pedro Henrique Karnoski <pedrokarnoski@gmail.com>
2024-04-01 21:06:40 -0300

    feat: Load fonts

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,29 @@
 import { Slot } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 import "../styles/global.css";
 
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import { Loading } from "../components/loading";
+
 export default function Layout() {
-  return <Slot />;
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+  });
+
+  if (!fontsLoaded) return;
+
+  return (
+    <>
+      <StatusBar style="light" />
+      {fontsLoaded ? <Slot /> : <Loading />}
+    </>
+  );
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,8 +2,8 @@ import { View,Text } from 'react-native';
 
 export default function Home() {
   return (
-    <View className=''>
-      <Text>Hello world!</Text>
+    <View className='flex-1 bg-green-500 items-center justify-center'>
+      <Text className='text-white text-2xl font-bold'>Hello world!</Text>
     </View>
   )
 }
